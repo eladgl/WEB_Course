dcd9155a2445978ca4c45e068338fc364a5c8f82
eladgl <59554824+eladgl@users.noreply.github.com>
2024-06-11 12:32:12 +0300

    creat new infrastructure for easy applying themes

--- tut2/src/scripts/components/button.js
+++ tut2/src/scripts/components/button.js
@@ -1,54 +1,65 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const numberBtns = document.querySelectorAll('.Btn-number');
-    const operatorBtns = document.querySelectorAll('.Btn-operator');
-    const notImplementedBtns = document.querySelectorAll('.Btn-notImplemented');
+document.addEventListener("DOMContentLoaded", (event) => {
+  const numberBtns = document.querySelectorAll(".Btn-number");
+  const operatorBtns = [
+    ...document.querySelectorAll(".Btn-operator"),
+    document.querySelector(".Btn-equal"),
+  ];
+  const notImplementedBtns = document.querySelectorAll(".Btn-notImplemented");
 
-    notImplementedBtns.forEach((btn => {
-        btn.addEventListener('click', (e) => {
-            window.showModal('Function not implemented yet');
-        });
-    }))
-
-    numberBtns.forEach((btn) => {
-        btn.addEventListener('click', (e) => {
-            window.label.innerHTML += e.target.value;
-            updateDecimalButtonState();
-        });
+  notImplementedBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      window.showModal("Function not implemented yet");
     });
+  });
 
-    operatorBtns.forEach((btn) => {
-        btn.addEventListener('click', (e) => {
-            switch(e.target.value) {
-                case '=': {
-                    try {
-                        window.label.textContent = eval(label.textContent);
-                    } catch (e) {
-                        window.showModal();
-                    }
-                    break;
-                }
-                case 'C': {
-                    window.label.textContent = '';
-                    break;
-                }
-                default: {
-                    window.label.textContent += e.target.value;
-                    break;
-                }
-            }
-            updateDecimalButtonState();
-        })
+  numberBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      window.label.innerHTML += e.target.value;
+      updateDecimalButtonState();
     });
+  });
 
-    const hasDecimalPoint = () => {
-        return window.label?.textContent?.includes('.');
-    };
+  operatorBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      switch (e.target.value) {
+        case "=": {
+          try {
+            window.label.textContent = eval(window.label.textContent);
+          } catch (e) {
+            window.showModal();
+          }
+          break;
+        }
+        case "C": {
+          window.label.textContent = "";
+          break;
+        }
+        case "BS": {
+          window.label.textContent = window.label.textContent.substring(
+            0,
+            window.label.textContent.length - 1
+          );
+          break;
+        }
+        default: {
+          console.log(e.target.value);
+          window.label.textContent += e.target.value;
+          break;
+        }
+      }
+      updateDecimalButtonState();
+    });
+  });
 
-    const updateDecimalButtonState = () => {
-            const decimalButton = document.querySelector('button[value="."]');
-            if (decimalButton) {
-                decimalButton.disabled = hasDecimalPoint();
-            }
-        };
-    updateDecimalButtonState();
+  const hasDecimalPoint = () => {
+    return window.label?.textContent?.includes(".");
+  };
+
+  const updateDecimalButtonState = () => {
+    const decimalButton = document.querySelector('button[value="."]');
+    if (decimalButton) {
+      decimalButton.disabled = hasDecimalPoint();
+    }
+  };
+  updateDecimalButtonState();
 });
--- tut2/tailwind.config.js
+++ tut2/tailwind.config.js
@@ -1,33 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    './src/**/*.{html,js, css}',
-  ],
+  darkMode: "class",
+  content: ["./src/**/*.{html,js, css}"],
   theme: {
     extend: {
       colors: {
-        dark: {
-          100: '#f0f0f0', // Lightest shade of dark mode
-          200: '#c0c0c0', // Light shade of dark mode
-          button: '#333'          
-        },
-        light: {
-          button: '#fff'
-        }
-      },
-      keyframes: {
-        slideInLeft: {
-          '0%': { transform: 'translateX(-100%)', opacity: '0' },
-          '50&': { transform: 'translate(-50%)', opacity: '0.5' },
-          '100%': { transform: 'translateX(0)', opacity: '1' },
-        },
-      },
-      animation: {
-        slideInLeft: 'slideInLeft 2s ease-out',
+        100: "var(--lighterDark)",
+        200: "var(--lightDark)",
+        background: "var(--background)",
+        border: "var(--border)",
+        "button-background-number": "rgb(var(--buttonBackgroundNumber))",
+        "button-background-operator": "rgba(var(--buttonBackgroundOperator))",
+        "button-background-equal": "rgba(var(--buttonBackgroundEqual))",
+        "button-text-color": "rgba(var(--buttonTextColor))",
+        "button-text-color-hover": "rgba(var(--buttonTextColorHover))",
+        text: "var(--text)",
+        row: "var(--row)",
+        textArea: "var(--textArea)",
+        modal: "var(--modal)",
+        modalOverlay: "rgba(var(--modalOverlay))",
       },
     },
+    keyframes: {
+      slideInLeft: {
+        "0%": { transform: "translateX(-100%)", opacity: "0" },
+        "50&": { transform: "translate(-50%)", opacity: "0.5" },
+        "100%": { transform: "translateX(0)", opacity: "1" },
+      },
+    },
+    animation: {
+      slideInLeft: "slideInLeft 2s ease-out",
+    },
   },
-  plugins: []
-}
-
+  plugins: [],
+};
